59ad13b51fea5ec21c68ffa42c7ecfb3c95fefee
Joseph Frank <>
2017-09-07 12:30:13 -0500

    git setup

---
+++ Swift-Language-Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Language-Basics
+//
+//  Created by Joseph Frank on 9/7/17.
+//  Copyright © 2017 Joseph Frank. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
